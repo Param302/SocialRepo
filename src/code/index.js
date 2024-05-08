62cbb63107de8c964bb54f81b4de66bee56d2968
ronakptl996 <pronak391@gmail.com>
2024-05-08 11:30:20 +0530

    style: navbar, edit modal

--- src/code/index.js
+++ src/code/index.js
@@ -1,1 +1,18 @@
 console.log("Social Repo opened!");
+
+document.addEventListener("DOMContentLoaded", function () {
+  const editButton = document.getElementById("edit-button");
+  const editPanel = document.getElementById("edit");
+
+  editButton.addEventListener("click", function () {
+    const isHidden = editPanel.classList.contains("hidden");
+
+    if (isHidden) {
+      editPanel.classList.remove("hidden");
+      editButton.classList.add("active");
+    } else {
+      editPanel.classList.add("hidden");
+      editButton.classList.remove("active");
+    }
+  });
+});
