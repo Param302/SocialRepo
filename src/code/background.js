4825439d6cfe3a7422a80bda297d34f9f06567e8
Parampreet Singh <rubercuber.30@gmail.com>
2024-05-11 02:42:12 +0530

    Commands for Input & Textarea working

--- src/code/background.js
+++ src/code/background.js
@@ -26,3 +26,49 @@ console.log("Background script!");
 //         }
 //     }
 // );
+
+
+chrome.runtime.onMessage.addListener(
+    function (request, sender, sendResponse) {
+        console.log("Reached background.js");
+        if (request) {
+            console.log(request);
+            if (request.message === "input changed") {
+                const inputs = Array.from(request.inputs);
+                console.log("INPUTS", inputs);
+                request.inputs.forEach((input) => {
+                    if (input.value[0] === "\\") {
+                        console.log("Found a backslash");
+                        switch (input.value) {
+                            case "\\twitter":
+                                console.log("Found twitter");
+                                sendResponse({ message: "Twitter found!", url: "https://twitter.com/Param3021" });
+                                break;
+                            case "\\github":
+                                console.log("Found GitHub");
+                                sendResponse({ message: "GitHub found!", url: "https://github.com/Param302" });
+                                break;
+                            case "\\linkedin":
+                                console.log("Found LinkedIn");
+                                sendResponse({ message: "LinkedIn found!", url: "https://www.linkedin.com/in/param302/" });
+                                break;
+                            case "\\email":
+                                console.log("Found email");
+                                sendResponse({ message: "Email found!", url: "connectwithparam.30@gmail.com" });
+                                break;
+                            case "\\socialrepo":
+                                console.log("Found socialrepo");
+                                sendResponse({ message: "Email found!", url: "Nope! It's SocialRepo 😉" });
+                                break;
+                            default:
+                                console.log("Invalid input");
+                                break;
+                        }
+                    }
+                });
+                sendResponse({ message: "DONE!" });
+            } else {
+                console.log("Did not receive the response!!!")
+            }
+        }
+    });
--- src/code/content.js
+++ src/code/content.js
@@ -2,30 +2,58 @@ console.log("CONTENT script");
 
 function getInputElements() {
     let arr = [];
-    let inputs = document.getElementsByTagName('input');
+    let inputs = [...document.getElementsByTagName("input"), ...document.getElementsByTagName("textarea")];
     for (let i = 0; i < inputs.length; i++) {
-        arr.push(
-            {
-                placeholder: inputs[i].placeholder,
-                value: inputs[i].value,
-            }
-        );
+        if (inputs[i].type == "text" || inputs[i].tagName == "TEXTAREA") {
+            arr.push(
+                {
+                    placeholder: inputs[i].placeholder,
+                    value: inputs[i].value,
+                }
+            );
+        }
     }
     return arr;
 }
 
 console.log(getInputElements());
-[...document.getElementsByTagName("input")].forEach((input) => {
-    input.addEventListener("input", () => {
+[...document.getElementsByTagName("input"), ...document.getElementsByTagName("textarea")].forEach((input) => {
+    input.oninput = () => {
+    // input.addEventListener("input", () => {
         console.log("Input changed!");
+        sendAndReceiveMessage().then((message) => {
+            if (message) {
+                console.log("Message: ", message, input.value);
+                input.value = message;
+            }
+        }).catch((error) => {
+            console.log("Error: ", error);
+        });
+    // });
+    };
+});
+
+function sendAndReceiveMessage() {
+    return new Promise((resolve, reject) => {
+        let url = "";
         chrome.runtime.sendMessage({
             message: "input changed",
             inputs: getInputElements(),
         }, (response) => {
             console.log("Response from index script: ", response);
+            try {
+                if (response.url) {
+                    url = response.url;
+                    console.log("URL: ", url);
+                }
+                resolve(url);
+            } catch (error) {
+                console.log("Error: ", error);
+                reject(error);
+            }
         });
     });
-});
+}
 
 // chrome.runtime.sendMessage({
 //     message: "hello!",
